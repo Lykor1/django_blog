c607b9045b7ec80131f7915f05b929820ee7c6c9
Evgen <evgeshkarach@gmail.com>
2025-04-15 15:08:50 +0400

    2 глава завершена

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -14,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250, verbose_name='Название')
-    slug = models.SlugField(max_length=250, verbose_name='Слаг')
+    slug = models.SlugField(max_length=250, unique_for_date='publish', verbose_name='Слаг')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', verbose_name='Автор')
     body = models.TextField(verbose_name='Тело')
     publish = models.DateTimeField(default=timezone.now, verbose_name='Дата публикации')
@@ -33,3 +34,33 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish']),
         ]
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ]
+        )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    name = models.CharField(max_length=80, verbose_name='Имя')
+    email = models.EmailField(verbose_name='Электронная почта')
+    body = models.TextField(verbose_name='Тело комментария')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+    active = models.BooleanField(default=True, verbose_name='Активна')
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
